6ead5a12a4901218e516b1a7ad8db4a4e9a7be8d
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2024-03-25 14:23:26 +0300

    - migration

---
+++ WeatherQuality/WeatherQuality.Infrastructure/Migrations/20240325111855_TimeZone.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeatherQuality.Telegram.Migrations
+{
+    /// <inheritdoc />
+    public partial class TimeZone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TimeZone",
+                table: "UserLocationModels",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeZone",
+                table: "UserLocationModels");
+        }
+    }
+}
