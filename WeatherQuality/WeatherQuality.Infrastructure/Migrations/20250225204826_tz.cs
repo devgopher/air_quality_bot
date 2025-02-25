05c79b3e7c66ed76272816cf6e29fbe1fcf97fd3
stone1985 <igevdo@mail.ru>
2025-02-25 23:49:09 +0300

    - tz bugfix

---
+++ WeatherQuality/WeatherQuality.Infrastructure/Migrations/20250225204826_tz.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeatherQuality.Telegram.Migrations
+{
+    /// <inheritdoc />
+    public partial class tz : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TimeZone",
+                table: "UserLocationModels",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateTable(
+                name: "ScheduleModels",
+                columns: table => new
+                {
+                    ChatId = table.Column<string>(type: "text", nullable: false),
+                    Cron = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleModels", x => new { x.ChatId, x.Cron });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScheduleModels");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TimeZone",
+                table: "UserLocationModels",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
